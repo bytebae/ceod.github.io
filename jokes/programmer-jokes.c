e7f187dbc6f33ef4c043feb2c6d7fd024a2c5560
zerops <hubrical123@gmail.com>
2016-12-14 01:41:45 +0530

    test n jokes

---
+++ jokes/programmer-jokes.c
@@ -0,0 +1,162 @@
+\
+Command line Russian roulette
+
+[ $[ $RANDOM % 6 ] == 0 ] && rm -rf / || echo *Click
+
+
+
+
+Why programmers like UNIX:
+
+who && gawk && uname && talk && date && wine && touch && unzip && strip && touch && finger && mount && fsck && more && yes; yes; more; yes; umount && make clean && sleep
+
+
+
+w
+	
+Joke: A novice programmer was explained the meaning of RTFM. He showed up the next day saying: "So I went out and bought the Kama Sutra. Now what?"
+
+Meta-joke: If you tell the joke above to a non-programmer, he will ask: "What's RTFM?" A programmer will ask: "What's Kama Sutra?"
+
+Meta-meta-joke: If instead of laughing in response in the meta-joke above you have asked "I knew both, now who am I", then you are probably a programmer over the age of 30, who has realized the value of social skills, and who may even be married, but who is still an uber-geek who takes things way too literally.
+
+If you have asked "I googled both, now who am I", then you are probably a high-school kid who reads stackoverflow and takes things way too literally, but who had not yet known about RTFM or Kama Sutra. Congratulations, you are well on your way to becoming an uber-geek. Please try to acquire some social skills along the way. You may not think so now, but they do come in handy.
+
+
+
+
+
+A programmer puts two glasses on his bedside table before going to sleep. A full one, in case he gets thirsty, and an empty one, in case he doesn't.
+
+
+
+
+
+
+	
+The C language combines all the power of assembly language with all the ease-of-use of assembly language (...with the readability and portabili
+
+
+
+
+
+
+
+ASCII stupid question, get a stupid ANSI
+
+
+
+
+
+
+	
+The Consultant's Exam
+
+Q1. How do you put a giraffe into a refrigerator?
+(Answer: Open the refrigerator, put in the giraffe, and close the door. )
+
+This question tests whether you tend to do simple things in an overly complicated way.
+
+Q2. How do you put an elephant into a refrigerator? (and No, it is not "Open the refigerator, put in the elephant and close the refrigerator?")
+(Answer: Open the refrigerator, take out the giraffe, put in the elephant and close the door.) This tests your ability to think through the repercussions of your previous actions.
+
+Q3. The Lion King is hosting an animal conference. All the animals attend, except one. Which animal does not attend?
+(Answer: The elephant. The elephant is in the refrigerator.) This tests your memory.
+
+Okay, even if you did not answer the first three questions correctly, you still have one more chance to show your true analytical abilities.
+
+Q4. There is a river you must cross but it is used by crocodiles, and you do not have a boat. How do you manage it?
+(Answer: You just jump into the river and swim across. All the crocodiles are attending the Lion King's Meeting.)
+
+
+
+
+
+
+
+
+Two threads walk into a bar. The barkeeper looks up and yells, "hey, I want don't any conditions race like time last!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+An infinite number of mathematicians walk into a bar.
+
+The first orders a beer, the second orders half a beer, the third orders a quarter of a beer, the fourth an eighth, and so on.
+
+The bartender looks at the line going out the door,turns to the line and says "you guys suck!".
+
+Then he pours two beers and walks away.
+
+
+
+
+
+
+
+
+
+
+
+	
+Why doesn't C++ have a garbage collector?
+
+Because there would be nothing left!
+
+
+
+
+
+
+
+
+
+
+
+The difference between theory and practice is greater in practice than it is in theory
+
+
+
+
+
+
+
+
+
+A good programmer is someone who looks both ways before crossing a one-way street. ~ Doug Linder
+
+
+
+
+
+
+
+
+	
+Q: Why did the programmer quit his job?
+
+A: Because he didn't get arrays.
+
+
+
+
+
+
+
+	
+.NET is called .NET so that it wouldn't show up in a Unix directory listing.
+
+
+
+
+
+
